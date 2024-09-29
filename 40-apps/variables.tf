d52983f6029c1333a95d6dbbe7129a9c3b1c938c
Rajprudvin <prudvirajn@gmail.com>
2024-09-29 12:28:04 +0530

    terraform

---
+++ 40-apps/variables.tf
@@ -0,0 +1,43 @@
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        project = "expense"
+        Terraform = "true"
+        environment = "dev"
+    }
+}
+
+variable "mysql_tags" {
+    default = {
+        component = "mysql"
+    }
+}
+
+variable "backend_tags" {
+    default = {
+        component = "backend"
+    }
+}
+
+variable "frontend_tags" {
+    default = {
+        component = "frontend"
+    }
+}
+
+variable "ansible_tags" {
+    default = {
+        component = "ansible"
+    }
+}
+
+variable "zone_name" {
+    default = "prudviraj.online"
+}
